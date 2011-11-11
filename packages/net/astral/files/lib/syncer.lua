44cf233bbc3fd4ee7e3e81ec2483465d84a00f1e
Koionai Codoranro <codoranro@gmail.com>
2011-11-11 16:12:59 +0600

    Change syncer update frequency

--- packages/net/astral/files/lib/syncer.lua
+++ packages/net/astral/files/lib/syncer.lua
@@ -3,7 +3,7 @@ local posix = require 'posix'
 local pid = posix.fork()
   if pid == 0 then
      while true do
-       posix.sleep(30)
+       posix.sleep(120)
        os.execute("/usr/lib/astral/gate-utils.sh sync_hosts > /dev/null 2>&1")
        os.execute("/usr/lib/astral/gate-utils.sh generate_config > /dev/null 2>&1")
        os.execute("/sbin/start-stop-daemon -p /var/run/astral-tincd.pid -s HUP -K > /dev/null 2>&1")
